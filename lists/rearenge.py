df4e74205c0782d968ec9cf6e2892b94e5221e4c
Marcos Lino <marcos.lino@AC-marcoslino.local>
2020-11-26 08:29:33 -0300

    recursive approach

--- lists/rearenge.py
+++ lists/rearenge.py
@@ -17,6 +17,26 @@ def rearrange(lst):
     return lst
 
 
+def r_rearrange(lst):
+    """
+    Recursive rearrange
+    """
+    def recursive_rearrange(lst, start, end):
+        if start >= end:
+            return lst
+        else:
+            if lst[start] >= 0 and lst[end] < 0:
+                lst[start], lst[end] = lst[end], lst[start]
+                return recursive_rearrange(lst, start + 1, end - 1)
+            else:
+                if lst[start] < 0:
+                    return recursive_rearrange(lst, start + 1, end)
+                if lst[end] >= 0:
+                    return recursive_rearrange(lst, start, end - 1)
+
+    return recursive_rearrange(lst, start=0, end=len(lst) - 1)
+
+
 def test_base():
     assert [] == rearrange([])
     assert [1] == rearrange([1])
@@ -29,10 +49,5 @@ def test_case1():
 
 
 def test_case2():
-    assert [-1, 1] == rearrange([1, -1])
-
-
-
-
-
-
+    assert [-6, -1, -9, 4, 5, 20, 10] == rearrange([10,-1,20,4,5,-9,-6])
+    assert [-6, -1, -9, 4, 5, 20, 10] == r_rearrange([10,-1,20,4,5,-9,-6])
